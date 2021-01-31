f5f0c469da2e72205f42c9c6c216d7865eed6c18
Alex <ashangin@hotmail.com>
2021-01-31 14:43:00 +0300

    Users can navigate between pages using tabbar

---
+++ Bot-Shop/PastOrderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PastOrderViewController.swift
+//  Bot-Shop
+//
+//  Created by Alex on 1/31/21.
+//
+
+import Foundation
+import UIKit
+
+
+class PastOrderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
