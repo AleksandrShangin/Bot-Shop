f1aa4504d037ab596b9f7e34795bf2b61d61dd21
Alex <ashangin@hotmail.com>
2021-01-31 15:31:00 +0300

    Set up basic collection view

---
+++ Bot-Shop/NewItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewItemCell.swift
+//  Bot-Shop
+//
+//  Created by Alex on 1/31/21.
+//
+
+import Foundation
+import UIKit
+
+class NewItemCell: UICollectionViewCell {
+    
+    
+    
+    
+}
