904d04b681dc5941e041e33c80967e238e9adcf4
Alex <ashangin@hotmail.com>
2021-01-31 12:35:11 +0300

    project init

---
+++ Bot-Shop/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Bot-Shop
+//
+//  Created by Alex on 1/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
